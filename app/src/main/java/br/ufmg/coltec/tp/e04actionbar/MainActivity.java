404f3d489e119b2ec7bdc1e477f449b3020baa4e
RitaRez <ritarezborges@gmail.com>
2018-04-08 19:30:46 -0300

    COmmit inicial

--- app/src/main/java/br/ufmg/coltec/tp/e04actionbar/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e04actionbar/MainActivity.java
@@ -1,7 +1,13 @@
 package br.ufmg.coltec.tp.e04actionbar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class );
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // infla menu na tela
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // recupera id do item selecionado
+        int id = item.getItemId();
+
+        // verifica qual é o botão selecionado com base no id
+        switch (id) {
+            case R.id.action_search:
+                Toast.makeText(this, "Clicou no search", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_refresh:
+                Toast.makeText(this, "Clicou no refresh", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(this, "Clicou no settings", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
