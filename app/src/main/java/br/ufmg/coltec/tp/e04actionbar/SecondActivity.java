761b870b1b4703d9ca6d280874e6054629903438
kingnowdo <reinaldomagas@gmail.com>
2018-04-17 22:12:55 -0300

    Entendendo o Up Navigation

--- app/src/main/java/br/ufmg/coltec/tp/e04actionbar/SecondActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e04actionbar/SecondActivity.java
@@ -6,15 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
 
-        Button botao = findViewById(R.id.botao1);
-        final Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+        Button botao = findViewById(R.id.botao2);
+        final Intent intent = new Intent(SecondActivity.this,ThirdActivity.class);
 
         botao.setOnClickListener(new View.OnClickListener() {
             @Override
