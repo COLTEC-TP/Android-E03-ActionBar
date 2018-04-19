dc9b70daf57f05b5f46feaca3f3f7df05b626bd6
kingnowdo <reinaldomagas@gmail.com>
2018-04-19 10:39:50 -0300

    Reaproveitando o que já existe

--- app/src/main/java/br/ufmg/coltec/tp/e04actionbar/SecondActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e04actionbar/SecondActivity.java
@@ -1,13 +1,17 @@
 package br.ufmg.coltec.tp.e04actionbar;
 
 import android.content.Intent;
+//import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
@@ -31,6 +35,33 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_second,menu);
+
+        MenuItem item = menu.findItem(R.id.item_buscar);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Toast.makeText(SecondActivity.this, "Buscar o texto: " + s, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                // executado enquanto texto é alterado pelo usuário
+                return false;
+            }
+        });
+
+
+        MenuItem shareItem = menu.findItem(R.id.item_compartilhar);
+        ShareActionProvider shareProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, "Baixe o meu aplicativo");
+
+        shareProvider.setShareIntent(intent);
+
 
         return super.onCreateOptionsMenu(menu);
     }
